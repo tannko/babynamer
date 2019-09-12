d91aa58cc45962ba8fd103e2806a5b05a746a0fb
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2019-09-12 09:46:15 -0400

    added form validation for share modal

--- frontend/src/ShareModal.js
+++ frontend/src/ShareModal.js
@@ -8,20 +8,28 @@ class ShareModal extends React.Component {
     super(props);
     this.state = { email: "" };
     this.handleInput = this.handleInput.bind(this);
-    this.handleSaveClick = this.handleSaveClick.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
   }
 
   handleInput(e) {
     this.setState({ email: e.target.value });
   }
 
-  handleSaveClick() {
-    this.props.share(this.state.email);
+  submitHandler = event => {
+    event.preventDefault();
+    event.target.className += " was-validated";
+    if (this.state.email.trim() !== "") {
+      this.props.share(this.state.email);
+    }
   }
 
   render() {
 
     return(
+      <form
+        className="needs-validation"
+        onSubmit={this.submitHandler}
+        noValidate>
       <MDBContainer>
         <MDBModal isOpen={this.props.modal} toggle={this.props.toggle} backdrop={false}>
           <MDBModalHeader toggle={this.props.toggle}>Share with your partner</MDBModalHeader>
@@ -29,15 +37,21 @@ class ShareModal extends React.Component {
             <MDBInput
               name="email"
               label="Enter your partner's email"
-              onInput={this.handleInput}>
+              onInput={this.handleInput}
+              required
+              pattern="\S+">
+              <div className="invalid-feedback">
+                Please enter email
+              </div>
             </MDBInput>
           </MDBModalBody>
           <MDBModalFooter>
             <MDBBtn color="secondary" onClick={this.props.toggle}>Cancel</MDBBtn>
-            <MDBBtn color="primary" onClick={this.handleSaveClick}>Save</MDBBtn>
+            <MDBBtn color="primary" type="submit">Save</MDBBtn>
           </MDBModalFooter>
         </MDBModal>
       </MDBContainer>
+      </form>
     );
   }
 }
