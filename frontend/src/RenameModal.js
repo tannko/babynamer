d84ac0a23c74d53000b0d9d7117a3b7cbd2114b2
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2019-09-12 10:54:12 -0400

    added form validation for rename modal

--- frontend/src/RenameModal.js
+++ frontend/src/RenameModal.js
@@ -7,21 +7,27 @@ class RenameModal extends React.Component {
     super(props);
     this.state = { newname: "" };
     this.handleInput = this.handleInput.bind(this);
-    this.handleSaveClick = this.handleSaveClick.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
   }
 
   handleInput(e) {
     this.setState({ newname: e.target.value });
   }
 
-  handleSaveClick() {
-    // save new name for the list
-    this.props.rename(this.state.newname);
+  submitHandler = event => {
+    event.preventDefault();
+    event.target.className += " was-validated";
+    if (this.state.newname.trim() !== "") {
+      this.props.rename(this.state.newname);
+    }
   }
 
   render() {
-
     return(
+      <form
+        className="needs-validation"
+        onSubmit={this.submitHandler}
+        noValidate>
       <MDBContainer>
         <MDBModal isOpen={this.props.modal} toggle={this.props.toggle} backdrop={false}>
           <MDBModalHeader toggle={this.props.toggle}>Rename list</MDBModalHeader>
@@ -29,15 +35,21 @@ class RenameModal extends React.Component {
             <MDBInput
               name="newname"
               label="Enter new name"
-              onInput={this.handleInput}>
+              onInput={this.handleInput}
+              required
+              pattern="\S+">
+              <div className="invalid-feedback">
+                Please add name
+              </div>
             </MDBInput>
           </MDBModalBody>
           <MDBModalFooter>
             <MDBBtn color="secondary" onClick={this.props.toggle}>Cancel</MDBBtn>
-            <MDBBtn color="primary" onClick={this.handleSaveClick}>Save</MDBBtn>
+            <MDBBtn color="primary" type="submit">Save</MDBBtn>
           </MDBModalFooter>
         </MDBModal>
       </MDBContainer>
+      </form>
     );
   }
 }
