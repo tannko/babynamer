f1a08a7f6ea9af3bc764ffc97686c265b996101b
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2019-09-20 19:45:05 -0400

    bug fixed

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -121,7 +121,6 @@ class SignUp extends React.Component {
               <div className="text-center mb-4 mt-5">
                 <MDBBtn
                   color="danger"
-                  type="button"
                   className="btn-block z-depth-2"
                   type="submit"
                 >
