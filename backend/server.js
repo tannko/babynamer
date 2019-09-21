d5bc5ad7288459c8698faa8780045b601f62c6d3
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2019-09-20 21:56:19 -0400

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,10 @@ function startServer() {
   loaders(app);
 
 
-  app.use(express.static(path.join(process.env.PWD + "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(process.env.PWD + "../client/build/index.html"));
+  app.use(express.static(path.join(process.env.PWD , "../client/build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    //res.sendFile(path.join(process.env.PWD , "../client/build/index.html"));
   });
 
   const server = http.createServer(app);
