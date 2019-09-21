8ba99268d33fb97eb7e3c3c77417742b4a705413
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2019-09-20 22:21:47 -0400

    bug fixed

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import PublicRoute from './components/PublicRoute';
 import PrivateRoute from './components/PrivateRoute';
 import MyLists from './components/MyLists';
 import SharedWithMe from './components/SharedWithMe';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 
 const routing = (
@@ -31,4 +31,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.register();
